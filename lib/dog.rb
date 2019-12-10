8d609c1965264a8d2e1419c4ea8d38ad70f4e5aa
Javier <jagi.ccs@gmail.com>
2019-12-10 11:01:52 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+    def initialize(name,breed = "Mutt")
+        @breed = breed
+        @name = name
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def name 
+        @name
+    end   
+end
+
